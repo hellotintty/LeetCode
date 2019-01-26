3bca36e3d17889ed51ee15169994fc32ad57a526
hellotintty <584387796@qq.com>
2019-01-26 10:23:52 +0800

    矩阵覆盖

---
+++ src/swordOffer/RectCover_1.java
@@ -0,0 +1,21 @@
+package swordOffer;
+
+/**
+ * @author tianqi
+ * @date 2019/1/26
+ * 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+ * 请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+ */
+public class RectCover_1 {
+    public int RectCover(int target) {
+        if (target == 0){
+            return 0;
+        }
+        else if (target == 1||target == 2){
+            return target;
+        }
+        else {
+            return RectCover(target-1)+RectCover(target-2);
+        }
+    }
+}
